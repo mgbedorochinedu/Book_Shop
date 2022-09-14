3e870ec9a604a64a3f8577ae2f4d9aab3db7d570
Mgbedoro Chinedu <69406478+mgbedorochinedu@users.noreply.github.com>
2022-09-14 12:21:12 +0100

    Setup unit testing environment

---
+++ Book-Shop-Test/PublisherServiceTest.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+
+namespace Book_Shop_Test
+{
+    public class PublisherServiceTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+
+    }
+}
